0d0eb41f56b77981b60ca59d8b85f01a78a0c2de
TheLetterSix <lmaclean234@gmail.com>
2018-04-03 13:11:03 -0400

    yeet

--- Home/models.py
+++ Home/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Kid(models.Model):
+	kid_name = models.CharField(max_length=40, default='None')
+	kid_grade = models.IntegerField(max_length=20, default=7)
+	kid_signed_in = models.BooleanField(default=False)
+
--- Home/views.py
+++ Home/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponseRedirect
+from .models import Kid
 
 # Create your views here.
+def index(request):
+	all_kids = Kid.objects.all()
+	name = ""
+	if request.method == "POST":
+		name = request.POST.get("name", None)
+
+		grade =request.POST.get("grade", None)
+		if grade is None:
+			grade = " "
+		if(name == 'delete_all'):
+			Kid.objects.all().delete()
+		elif (len([x for x in all_kids if x.kid_name == name]) == 0):
+			newkid = Kid()
+			newkid.kid_name = name
+			newkid.kid_grade = grade
+			newkid.save()
+			return HttpResponseRedirect('/')
+		else:
+			kid = Kid.objects.get(kid_name=name)
+			setattr(kid, 'kid_signed_in', True)
+			kid.save()
+			return HttpResponseRedirect('/')
+
+	return render(request, "Home/index.html", {'all_kids' : all_kids, 'kids_signed_in' : [x for x in all_kids if x.kid_signed_in == True]})
