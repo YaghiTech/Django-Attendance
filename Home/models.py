bcdfe4374bb7e8e3bf7ab98272e246ab4ec587db
TheLetterSix <lmaclean234@gmail.com>
2018-04-11 13:54:09 -0400

    sign in and out

--- Home/models.py
+++ Home/models.py
@@ -5,15 +5,20 @@ class Kid(models.Model):
 	kid_name = models.CharField(max_length=40, default='None')
 	kid_grade = models.IntegerField(default=7)
 	kid_signed_in = models.BooleanField(default=False)
+	time_entered = models.DateTimeField(auto_now=True)
 
 class Teacher(models.Model):
 	techer_name = models.CharField(max_length=40, default='None')
 
 class SignIn(models.Model):
-	kid = models.OneToOneField(
-       Kid,
-        on_delete=models.CASCADE,
-	)
+	
+
+	kid_name = models.CharField(max_length=40, default='None')
+	kid_grade = models.IntegerField(default=7)
+	kid_id =  models.IntegerField(default=7)
+
+	teacher_name = models.CharField(max_length=40, default='None')
+
 #	teacher = models.OneToOneField(
 #		Teacher,
 #        on_delete=models.CASCADE,
@@ -21,6 +26,7 @@ class SignIn(models.Model):
 
 	time_entered = models.DateTimeField(auto_now=True)
 	time_left = models.DateTimeField(auto_now=True)
+	currently_signed_in = models.BooleanField(default=True)
 
 	subject = models.CharField(max_length=40, default='None')
 	place = models.CharField(max_length=40, default='None')
--- Home/views.py
+++ Home/views.py
@@ -11,6 +11,12 @@ def index(request):
 			if 'signout_'+str(kid.id) in request.POST:
 				setattr(kid, 'kid_signed_in', False)
 				kid.save()
+
+				sign_in = SignIn.objects.get(kid_id=kid.id,currently_signed_in=True)
+				setattr(sign_in, 'currently_signed_in', False)
+				setattr(kid, 'time_left', datetime.datetime.now())
+				sign_in.save()
+
 				#new_sign_in = SignIn(kid=kid)
 				#new_sign_in.kid = kid
 				#new_sign_in.save()
@@ -27,23 +33,33 @@ def index(request):
 		elif (len([x for x in all_kids if x.kid_name == name]) == 0):
 			newkid = Kid()
 			newkid.kid_name = name
+			newkid.kid_signed_in = True
 			newkid.save()
 			return HttpResponseRedirect('/')
 		else:
 			kid = Kid.objects.get(kid_name=name)
 			setattr(kid, 'kid_signed_in', True)
+			setattr(kid, 'time_entered', datetime.datetime.now())
+
 			kid.save()
-			new_sign_in = SignIn(kid=kid)
+
+			new_sign_in = SignIn()
+			new_sign_in.kid_name = kid.kid_name
+			new_sign_in.kid_grade = kid.kid_grade
 			new_sign_in.reason = request.POST.get("reason", None)
 			new_sign_in.subject = request.POST.get("subject", None)
-			new_sign_in.time_enterered = datetime.datetime.now()
+			new_sign_in.time_entered = datetime.datetime.now()
+			new_sign_in.kid_id = kid.id
+			#new_sign_in.time_left = datetime.datetime.now()
+
 			new_sign_in.save()
+		
 			return HttpResponseRedirect('/')
 
 		
 		
 	return render(request, "Home/index.html", {'all_kids' : all_kids,
-					 'kids_signed_in' : [x for x in all_kids if x.kid_signed_in == True], 'all_teachers' : Teacher.objects.all(),
+					 'kids_signed_in' : [x for x in SignIn.objects.all() if x.currently_signed_in == True], 'all_teachers' : Teacher.objects.all(),
 					 'all_sign_ins' : SignIn.objects.all})
 
 
